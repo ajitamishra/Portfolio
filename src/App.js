ab4e6511f2ab65a5680b65f4774a42ff67cb1fb7
ajitamishra <ajita00054@gmail.com>
2020-11-06 10:58:20 +0530

    Added carousel section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Navbar from './components/Navbar/myNavbar'
-
+import Mycarousel from './components/mycarousel/mycarousel';
+import Title from './components/title/title'
 function App() {
   return (
     <div className="App">
      <Navbar />
+     <Mycarousel/>
+     <Title/>
     </div>
   );
 }
